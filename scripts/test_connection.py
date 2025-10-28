d1282d944ca6cd8eb355f2ce618e736968020253
rtima <roschintimur@gmail.com>
2025-10-28 23:51:49 +0300

    added test script:   scripts/test_connection.py

---
+++ scripts/test_connection.py
@@ -0,0 +1,21 @@
+from config.database import get_db, engine
+from models.order_model import Order
+
+def test_db():
+    try:
+        # Тест подключения
+        with engine.connect() as conn:
+            print("it works")
+        
+        # Тест создания таблиц
+        with get_db() as db:
+            orders = db.query(Order).all()
+            print(f"it works. total orders: {len(orders)}")
+            
+        print("all good!")
+        
+    except Exception as e:
+        print(f"error: {e}")
+
+if __name__ == "__main__":
+    test_db()
