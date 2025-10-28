41da395fd8296369cb94793444cb982833236444
rtima <roschintimur@gmail.com>
2025-10-28 22:51:29 +0300

    new file:   models/inventory_model.py 	new file:   repositories/inventory_repository.py 	new file:   repositories/menu_repository.py 	deleted:    scripts/db_init.py

--- scripts/db_init.py
+++
@@ -1,9 +0,0 @@
-from config.database import engine, Base
-from models import Order, MenuItem, Inventory, OrderItem, Recipe
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    init_db()
-    print("Database tables created successfully")
