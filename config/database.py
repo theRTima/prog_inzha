136185cea282d539cd73d5fd41f25c59786b9b94
rtima <roschintimur@gmail.com>
2025-10-28 22:10:19 +0300

    modified:   config/database.py

--- config/database.py
+++ config/database.py
@@ -1,1 +1,18 @@
- 
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from contextlib import contextmanager
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/restaurant_db")
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
