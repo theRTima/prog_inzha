1550db00c73268f4660257bd0891fd85645138a9
rtima <roschintimur@gmail.com>
2025-10-28 23:00:23 +0300

    modified:   config/database.py 	modified:   models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,3 @@
 from .order_model import Order, OrderItem
 from .menu_model import MenuItem
 from .inventory_model import Inventory
-
-# Или если хотите импортировать все модели сразу:
-# from .order_model import *
-# from .menu_model import *
-# from .inventory_model import *
